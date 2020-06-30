2ad57846f35599c05cd41d7272110cd374904881
nugulee <das1218@naver.com>
2020-07-01 00:30:05 +0900

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+//차례대로 색을 바꾸는 것을 시도했다...실패했다. 
+// window.addEventListener("scroll", function(){
+//     var scroll=window.pageYOffset;
+//     console.log(scroll);
+
+//     var n=1;
+//     while(n<=8){
+
+//     var col1=document.getElementsByClassName('box')[n-1].style.backgroundColor;
+//     var col2=document.getElementsByClassName('box')[n].style.backgroundColor;  
+  
+//     if (scroll=50*n){
+//         document.getElementsByClassName('box')[n-1].style.backgroundColor=col2;
+//         // document.getElementsByClassName('box')[n].style.backgroundColor=col1;        
+        
+//     }   
+//     n++;
+//     console.log(n);
+//     }
+    
+// });
+
+window.addEventListener('scroll',function(){
+
+    var scroll = window.pageYOffset;
+    console.log(scroll);
+    var n=0;
+    while(n<8){
+        if (scroll>40+10*n&&scroll<40+20*n){
+        document.getElementsByClassName('box')[n].style.visibility='visible'}
+        n++;
+        } 
+});
+
+
+
+
